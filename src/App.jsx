0418a707b748adc802d76804a2133cbee144776c
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-01-11 22:52:18 +0400

    Add Header parth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Body from './Components/Body/Body'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <div>
-
+      <Header />
+      <Body />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { TbWorld } from "react-icons/tb";
+import { CiUser } from "react-icons/ci";
+
+const arr = ["Գլխավոր", "Զեղչեր", "Ծառայություններ", "Մեր մասին"]
+
+function Header() {
+    return (
+        <div className='flex justify-between items-center mt-[22px] w-[1300px]'>
+            <div>
+                <img src='https://amaranoc.am/images/logo.svg' className='w-[170px]' />
+            </div>
+            <div className='w-[550px] flex justify-between items-center'>
+                {arr.map((el,i) => {
+                    return <p key={i} className='pb-[5px] text-[18px] text-text3 md:text-text2 whitespace-nowrap text-secondary hover:border-b-[1px] hover:border-[#fd993a]'>{el}</p>
+                })}
+            </div>
+            <div className='flex justify-evenly items-center w-[400px]'>
+            <TbWorld className='text-[24px]' />
+            <CiUser className='text-[24px]'/>
+                <input 
+                type="text" 
+                placeholder='Որոնում' 
+                className='px-[35px] py-[8px] rounded-[25px] border-[1px] border-zinc-400' />
+            </div>
+        </div>
+    )
+}
+
+export default Header
