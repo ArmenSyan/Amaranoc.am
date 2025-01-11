4b2b3d4ff6463c8f9466e1fa0ebfffbdb3837928
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-01-12 00:29:17 +0400

    Create DataBase

---
+++ src/Data.js
@@ -0,0 +1,112 @@
+const DataBase = [{
+    theBest: [{
+        place: "Արզնի",
+        people: "50",
+        price: "150,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1719483738178--0.17939100328131374image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "30",
+        price: "130,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1727006467456--0.9643954207029866image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "20",
+        price: "150,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1705829500856--0.9156560389221753image.webp&w=1920&q=75",
+    }, {
+        place: "Պտղնի",
+        people: "30",
+        price: "100,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1733690961413--0.3657082475795794image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "15",
+        price: "100,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1718628350198--0.58103336918447image.webp&w=1920&q=75",
+    }, {
+        place: "Ակունք",
+        people: "40",
+        price: "90,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735922464506--0.41768509663126063image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "30",
+        price: "120,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1732618649852--0.9131125780896108image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "30",
+        price: "120,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1732618649852--0.9131125780896108image.webp&w=1920&q=75",
+    }, {
+        place: "Նոր Հաճն",
+        people: "25",
+        price: "108,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1712326262412--0.25440242535580326image.webp&w=1920&q=75",
+    },],
+    simple: [{
+        place: "Ծաղկաձոր",
+        people: "20",
+        price: "100,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1736346552283--0.4154739994823833image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "8",
+        price: "55,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1736346067846--0.48136685223654463image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "6",
+        price: "45,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1736264307372--0.5923133238359417image.webp&w=1920&q=75",
+    }, {
+        place: "Ծաղկաձոր",
+        people: "8",
+        price: "65,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1736262949713--0.5438384878532811image.webp&w=1920&q=75",
+    }, {
+        place: "Արզական",
+        people: "10",
+        price: "60,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735309341998--0.18031243054065027image.webp&w=1920&q=75",
+    }, {
+        place: "Քասախ",
+        people: "35",
+        price: "120,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735307864481--0.8688546137086155image.webp&w=1920&q=75",
+    }, {
+        place: "Դիլիջան",
+        people: "8",
+        price: "25,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735302502215--0.230355006263677image.webp&w=1920&q=75",
+    }, {
+        place: "Վանաձոր",
+        people: "6",
+        price: "30,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735210751295--0.852475450909614image.webp&w=1920&q=75",
+    }, {
+        place: "Թեղենիք",
+        people: "6",
+        price: "25,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735208216841--0.47958082625854104image.webp&w=1920&q=75",
+    }, {
+        place: "Փարպի",
+        people: "40",
+        price: "100,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1735208216841--0.47958082625854104image.webp&w=1920&q=75",
+    }, {
+        place: "Ջերմուկ",
+        people: "3",
+        price: "35,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1733224666202--0.8153535162596828image.webp&w=1920&q=75",
+    }, {
+        place: "Ջերմուկ",
+        people: "6",
+        price: "45,000 ֏",
+        imgUrl: "https://amaranoc.am/_next/image?url=https%3A%2F%2Fapi.amaranoc.am%2Fcompressed_images%2Fcompressed_1733224168744--0.13257965616354328image.webp&w=1920&q=75",
+    },]
+}]
+console.log(DataBase[0].theBest[0].place)
+
+export default DataBase
