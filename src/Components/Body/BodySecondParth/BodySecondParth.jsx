1360d70351023e6e15ec4530ffb9686a8aee603e
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-01-12 00:03:04 +0400

    add Map path

--- src/Components/Body/BodySecondParth/BodySecondParth.jsx
+++ src/Components/Body/BodySecondParth/BodySecondParth.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import BodySecond1 from './BodySecond1'
-import BodySecond2 from './BodySecond2'
 
 function BodySecondParth() {
   return (
     <div className='w-[1100px] flex flex-col justify-evenly pr-[10px] pl-[30px]'>
         <BodySecond1 />
-        <BodySecond2 />
     </div>
   )
 }
