2d0ef868c20608569363991df9fd71bef21da465
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-01-12 01:14:03 +0400

    Add Footer part

--- src/Components/Footer/FooterBottom.jsx
+++ src/Components/Footer/FooterBottom.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function FooterBottom() {
   return (
-    <div>
-        
+    <div className='w-[98.9vw] h-[200px] bg-[#101623] flex justify-evenly items-end'>
+      <img src='https://amaranoc.am/_next/image?url=%2Fimages%2Ffooter%2Ffooter-background.png&w=1920&q=75' />
     </div>
   )
 }
--- src/Components/Footer/FooterTop.jsx
+++ src/Components/Footer/FooterTop.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 function FooterTop() {
   return (
-    <div>
-        
+    <div className='mt-[150px] w-[98.9vw] h-[200px] bg-[#101623] flex flex-col justify-evenly items-center text-[white] '>
+    <div className='mt-[40px]'>
+        <p className='text-[40px] font-extrabold'>ԿՈՆՏԱԿՏՆԵՐ</p>
     </div>
+    <div className='flex justify-evenly items-center w-[1200px] mt-[30px]'>
+        <div className='hover:cursor-pointer flex justify-evenly items-center'>
+            <i className="fa fa-phone "></i>
+            <p className='ml-[10px]'>041-611-611/044-611-611</p>
+        </div>
+        <div className='hover:cursor-pointer flex justify-evenly items-center'>
+            <i className="fa fa-envelope "></i>
+            <p className='ml-[10px]'>address.amaranoc@gmail.com</p>
+        </div>
+        <div className='hover:cursor-pointer flex justify-evenly items-center'>
+            <i className="fa fa-instagram "></i>
+            <p className='ml-[10px]'>amaranoc.am</p>
+        </div>
+        <div className='hover:cursor-pointer flex justify-evenly items-center'>
+            <i className="fa fa-facebook "></i>
+            <p className='ml-[10px]'>amaranoc.am</p>
+        </div>
+        <div className='hover:cursor-pointer flex justify-evenly items-center'>
+            <i className="fa fa-map-marker "></i>
+            <p className='ml-[10px]'>Թումանյան 5</p>
+        </div>
+    </div>
+    <div>
+        <p className='hover:cursor-pointer mt-[50px] border-b-[1px] border-white'>Գաղտնիության քաղաքականություն</p>
+    </div>
+</div>
   )
 }
 
